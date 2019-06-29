b84e2b6f7d889704e7143301223b7b4c22ade023
Nan Chen <nanx2483@hotmail.com>
2019-06-30 01:42:45 +0200

    fixed factorial

--- factorial.cpp
+++ factorial.cpp
@@ -3,21 +3,35 @@
 using namespace std;
 
 class Factorial {
+private:
+	int result;
 public:
 	int getNum(int num) {
+		this->result = factorial(num);
+		return this->result;
+	}
+	int printUpTo(int num) {
+		for (int i = 0; i < num; i++) {
+			cout << "fac(" << i << ") = " << factorial(i) << endl;
+		}
+	}
+	
+
+	int factorial(int num) {
 		if (num < 0)
 			return 0;
 		if (num == 0 || num == 1)
 			return 1;
 		else
-			return getNum(num - 1) + getNum(num - 2); 
+			return num * factorial(num - 1); 
 	}
 
-}
+};
 
 int main() { 
 	Factorial f;
 	cout << f.getNum(2) << endl;
+	f.printUpTo(10);
 	return 0;
 }
 
